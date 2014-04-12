93e301a660dc84312f90d59b8f0da7edca1b9a81
Claire's 맥프레 <gkhackers@gmail.com>
2014-04-12 16:58:06 +0900

    .

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,10 +5,14 @@ public class Fibonacci{
 		System.out.println("You should put a pramameter.");
 		System.exit(0);
 	}
-        int start = Integer.parseInt(args[0]);
-        long fib1 = 0;
+        
+	int start = Integer.parseInt(args[0]);
+        
+	#if (start < 2)
+	long fib1 = 0;
         long fib2 = 1;
-        for(int i = 0; i <= (start -2); i++) {
+        
+	for(int i = 0; i <= (start -2); i++) {
             long fib3 = fib1 + fib2;
             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
             fib1 = fib2;
