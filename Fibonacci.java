0e37c7571bb37bcd2080f34f92bce3e660ac8e69
Claire's 맥프레 <gkhackers@gmail.com>
2014-04-12 15:44:23 +0900

    updated : Fibonacci.java errors has modified
    comment : I changed variable type 'int' into 'long" and check if args.length is zero. I feel failed to make codes with Java.
    
    Signed-off-by: Kyongjin Seo <gkhackers@gmail.com>

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,15 @@
 public class Fibonacci{
     public static void main(String []args){
+	if(args.length == 0)
+	{
+		System.out.println("You should put a pramameter.");
+		System.exit(0);
+	}
         int start = Integer.parseInt(args[0]);
-        int fib1 = 0;
-        int fib2 = 1;
+        long fib1 = 0;
+        long fib2 = 1;
         for(int i = 0; i <= (start -2); i++) {
-            int fib3 = fib1 + fib2;
+            long fib3 = fib1 + fib2;
             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
             fib1 = fib2;
             fib2 = fib3;
